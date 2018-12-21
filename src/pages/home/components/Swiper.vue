ce7c42bb24d5df8b8e9438c0b082c1e30dd94137
张红军 <zhanghongjun228@163.com>
2018-12-21 21:39:31 +0800

    添加swiper

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -23,11 +23,11 @@
         },
         swiperList: [
           {
-            id:'001',
-            imgUrl:'https://imgs.qunarzz.com/p/tts7/1805/51/8db4f954e22ca02.jpg_r_390x260x90_df87ae37.jpg'
+            id: '001',
+            imgUrl: 'https://imgs.qunarzz.com/p/tts7/1805/51/8db4f954e22ca02.jpg_r_390x260x90_df87ae37.jpg'
           }, {
-            id:'002',
-            imgUrl:'https://imgs.qunarzz.com/p/tts0/1806/6d/b776b0b2da64e502.jpg_r_390x260x90_550834a8.jpg'
+            id: '002',
+            imgUrl: 'https://imgs.qunarzz.com/p/tts0/1806/6d/b776b0b2da64e502.jpg_r_390x260x90_550834a8.jpg'
           }
         ]
       }
